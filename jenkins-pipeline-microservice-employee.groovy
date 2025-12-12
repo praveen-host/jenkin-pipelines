70b155ffe5608a36a8011831c48c441eadbc20c5
Praveen Yadav <myinbox.praveen@gmail.com>
2025-12-12 15:09:23 +0530

    Push image stage is added to pipeline

--- jenkins-pipeline-microservice-employee.groovy
+++ jenkins-pipeline-microservice-employee.groovy
@@ -23,17 +23,17 @@ pipeline {
             }
         }
         stage('login to DockerHub') {
-            // steps {
-            //     withcredentials([usernamePassword(credentialsId: 'dockerhub-creds', passwordVariable: 'DOCKER_CREDENTIALS_PSW', usernameVariable: 'DOCKER_CREDENTIALS_USR')]) {
-            //         sh "echo ${DOCKER_CREDENTIALS_PSW} | docker login -u ${DOCKER_CREDENTIALS_USR} --password-stdin"
-            //     }
-            // }
             steps {
                 withCredentials([usernamePassword(credentialsId: 'dockerhub-creds', usernameVariable: 'DOCKER_USER', passwordVariable: 'DOCKER_PASS')]) {
                     sh """
                 echo $DOCKER_PASS | docker login -u $DOCKER_USER --password-stdin
             """
                 }
+            }
+        }
+        stage('Push image to docker hub') {
+            steps {
+                sh "docker push ${DOCKER_IMAGE}:${DOCKER_TAG}"
             }
         }
     }
