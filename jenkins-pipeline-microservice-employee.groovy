cc457e733941e58a3ea67abea2271fd6b5fee965
Praveen Yadav <myinbox.praveen@gmail.com>
2025-12-12 14:21:14 +0530

    On branch master
    Initial commit
    
    Changes to be committed:
    	new file:   jenkins-pipeline-microservice-employee.groovy

---
+++ jenkins-pipeline-microservice-employee.groovy
@@ -0,0 +1,33 @@
+/* groovylint-disable CompileStatic */
+pipeline {
+    agent any
+    environment {
+        DOCKER_IMAGE = 'yadavpk/microservice-employee'
+        DOCKER_TAG = 'latest'
+        DOCKER_CREDENTIALS = credentials('dockerhub-creds')
+    }
+    stages {
+        stage('Checkout From Master Branch') {
+            steps {
+                git branch: 'main', url: 'https://github.com/praveen-host/microservice-employee.git'
+            }
+        }
+        stage('Restore Dependencies') {
+            steps {
+                sh 'dotnet restore microservice-employee.csproj'
+            }
+        }
+        stage('Build') {
+            steps {
+                sh 'dotnet build  microservice-employee.csproj --configuration release'
+            }
+        }
+        stage('login to DockerHub') {
+            steps {
+                withcredentials([usernamePassword(credentialsId: 'dockerhub-creds', passwordVariable: 'DOCKER_CREDENTIALS_PSW', usernameVariable: 'DOCKER_CREDENTIALS_USR')]) {
+                    sh "echo ${DOCKER_CREDENTIALS_PSW} | docker login -u ${DOCKER_CREDENTIALS_USR} --password-stdin"
+                }
+            }
+        }
+    }
+}
